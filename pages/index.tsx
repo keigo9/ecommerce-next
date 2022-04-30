f558e0fbdcac719ffcea769edf6728d5bf91538d
keigo9 <keigo.matsunari@gmail.com>
2022-04-30 13:20:13 +0900

    fetch local api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
-export default function Home() {
+import type { InferGetStaticPropsType } from "next"
+import getAllProduct from "@framework/product/get-all-product"
 
-  const message: string = "hello world"
+export async function getStaticProps() {
+  const products = await getAllProduct();
+  
+  return { 
+    props: {
+      products
+    },
+    revalidate: 4 * 60 *60
+  }
+}
+
+export default function Home({
+  products
+}: InferGetStaticPropsType<typeof getStaticProps>) {
 
   return (
     <div>
-      {message}
+      {JSON.stringify(products)}
     </div>
   )
 }
